cb350553a1c97a73834505a9a042e762dd5ed187
Axel <meister.axel@gmail.com>
2024-06-20 22:34:07 +0200

    fixed bug with dark mode toggle

--- src/components/modetoggle.tsx
+++ src/components/modetoggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
 
@@ -14,11 +14,7 @@ export function ModeToggle() {
 
   return (
     <Button variant="outline" size="icon" onClick={toggleTheme}>
-      {theme === "dark" ? (
-        <SunIcon className="h-[1.2rem] w-[1.2rem]" />
-      ) : (
-        <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
-      )}
+      <SunIcon className="h-[1.2rem] w-[1.2rem]" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
